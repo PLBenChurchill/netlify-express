ab2ba3653efb3ea5cc74e7428b4f940b150f9915
= <ben.churchill@playerlands.com>
2021-09-23 16:39:08 +0100

    deabstraction

--- express/server.js
+++ express/server.js
@@ -4,8 +4,19 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-import {saveSave} from "./functions/saveSave"
-import {getSave} from "./functions/getSave"
+import faunadb from "faunadb"
+const functions = {
+  Ref: faunadb.query.Ref,
+  Paginate: faunadb.query.Paginate,
+  Get: faunadb.query.Get,
+  Match: faunadb.query.Match,
+  Select: faunadb.query.Select,
+  Index: faunadb.query.Index,
+  Create: faunadb.query.Create,
+  Collection: faunadb.query.Collection,
+  Join: faunadb.query.Join,
+  Call: faunadb.query.Call,
+} = faunadb.query;
 
 const router = express.Router();
 
@@ -28,6 +39,31 @@ app.get("/get/:saveName", async (req, res) => {
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+
+export async function saveSave(client, functions, save) {
+  const doc = await client.query(
+    functions.Create(
+      functions.Collection('saves'),
+      {
+        data: save
+      }
+    )
+  );
+  return doc;
+}
+
+export async function getSave(client, saveName) {
+  try {
+    return await client.query(
+      functions.Get(functions.Match(functions.Index('saves_by_name'), [saveName]))
+    )
+  } catch (e) {
+    console.log(e)
+    return {};
+  }
+}
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
